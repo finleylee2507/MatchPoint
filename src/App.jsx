e6efd5118ea4aa029c123b6fbc28a9f7bdf4b1c3
Finley Li <li.f@wustl.edu>
2023-01-08 16:20:58 -0600

    Fix styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import EventCard from './components/events/Event';
-import eventData from './utilities/data/eventData.json';
+import {useState} from 'react'
+import './App.css'
+import EventCard from './components/events/Event'
+import eventData from './utilities/data/eventData.json'
+
 const App = () => {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      
-      {eventData.events.map(e => (<EventCard key={e.id} cardData={e}/>))}
-      
-    </div>
-  );
-};
+    return (
+        //TODO: these should technically be in event list
+        <div className="App" style={_styles.eventList}>
 
-export default App;
+            {eventData.events.map(e => (<EventCard key={e.id} cardData={e}/>))}
+
+        </div>
+    )
+
+
+}
+
+const _styles = {
+    eventList: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    }
+}
+
+export default App
--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -1,7 +1,7 @@
 // Event Card
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 import Basketball from '../../assets/Basketball.png'
 
 const EventCard = ({cardData}) => {
@@ -9,72 +9,68 @@ const EventCard = ({cardData}) => {
 
     return (
         <Card style={_styles.container}>
-          <CircularImageWrapper imageSrc={Basketball}/>
-          <div style={{}}>
-            <Card.Body style={_styles.cardContainer}>
-                <Card.Title>Title: {name}</Card.Title>
-                <Card.Text>
-                    Location: {location}
-                </Card.Text>
-                <Card.Text>
-                    Total spots: {max_cap}
-                </Card.Text>
-                <Card.Text>
-                    
-                    Spots avaliable: {max_cap-cur_cap}
-                </Card.Text>
-                <Card.Text>
-                    
-                    Date: {datetime.date}
-                </Card.Text>
-                
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </div>
+            <CircularImageWrapper imageSrc={Basketball}/>
+            <div style={{}}>
+                <Card.Body style={_styles.cardContainer}>
+                    <Card.Title>Title: {name}</Card.Title>
+                    <Card.Text>
+                        Location: {location}
+                    </Card.Text>
+                    <Card.Text>
+                        Total spots: {max_cap}
+                    </Card.Text>
+                    <Card.Text>
+
+                        Spots avaliable: {max_cap - cur_cap}
+                    </Card.Text>
+                    <Card.Text>
+
+                        Date: {datetime.date}
+                    </Card.Text>
+
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </div>
 
         </Card>
-      );
+    )
 }
 
 export const CircularImageWrapper = ({imageSrc}) => {
     return (
         <div style={_styles.circularImageWrapperContainer}>
             <img style={_styles.image} src={imageSrc}/>
-        </div>  
+        </div>
     )
 }
 
 const _styles = {
     cardContainer: {
-
         borderRadius: '20px',
-        
-
     },
     container: {
-        margin:'20px',
+        margin: '20px',
         display: 'flex',
-        flexDirection: 'row', 
-        justifyContent:'space-around',
-        width:'50vw'
-  
-        
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '40vw',
+
     },
     circularImageWrapperContainer: {
         // border: '1px solid black',   
-        display:'flex',
-        flexDirection:'column',
-        justifyContent:'center',
-        alighItems:'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alighItems: 'center',
 
     },
     image: {
-        width:'150px',
-        height:'auto',
-        borderRadius:'50%'
+        width: '150px',
+        height: 'auto',
+        borderRadius: '50%'
     }
 
 }
 
-export default EventCard;
+export default EventCard
 
