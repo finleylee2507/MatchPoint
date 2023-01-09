84841f0193a06b122fd7fc1ca69f4c59bbb16a90
unknown <chehongle123@163.com>
2023-01-09 16:33:48 -0600

    backbone of navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Nav, Form, Navbar, NavDropdown, Offcanvas, Button, Container } from 'react-bootstrap';
+
+function NavBar() {
+    return (
+        <>
+            <Navbar bg="light" expand="false" className="mb-3">
+                <Container fluid>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar`} />
+                    <Navbar.Brand href="#">MatchPoint</Navbar.Brand>
+                    <Nav.Link href="#action1">Sign in</Nav.Link>
+                    <Navbar.Offcanvas
+                        id={`offcanvasNavbar`}
+                        aria-labelledby={`offcanvasNavbarLabel`}
+                        placement="start"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel`}>
+                                MatchPoint
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <Nav.Link href="#action1">Home</Nav.Link>
+                                <Nav.Link href="#action2">Link</Nav.Link>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+export default NavBar;
