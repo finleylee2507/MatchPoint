84718e3486b099d5f454f1e0bc3b2fff88285fbf
Evan <evanmwaite@gmail.com>
2023-01-12 08:47:07 -0600

    Replaced hard-coded data on modal with data from first event

--- src/components/events/EventModal.jsx
+++ src/components/events/EventModal.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import {Button, Modal} from 'react-bootstrap'
 
 function EventModal({show, handleClose, data}) {
+    const {name, location, max_cap, cur_cap, datetime, img_src} = data
     return (
         <Modal show={show} onHide={handleClose} info={data} centered>
             <Modal.Header closeButton>
-                <Modal.Title>Pickup Soccer Game</Modal.Title>
+                <Modal.Title>{name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
+                <p>Location: {location}</p>
                 <p>Participants: Vedant, Finley, Rawan, Vincent</p>
-                <p>Spots Available: 18</p>
+                <p>Spots Available: {max_cap - cur_cap}</p>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="primary" onClick={handleClose}>
