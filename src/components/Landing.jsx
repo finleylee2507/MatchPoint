be071a13d2fb89e24d560d77f76b864f65a08b75
Finley Li <li.f@wustl.edu>
2023-01-12 22:54:55 -0600

    Redesigned landing page. Not mobile responsive currently.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,12 +3,19 @@
 import { signInWithGoogle, signOut, useAuthState } from "../utilities/firebase";
 import { Button } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
+import './Landing.css';
+import Image from "react-bootstrap/Image"
+import appImage from "../assets/app.png"
+import smallLogo from "../assets/small-logo.png"
 
-const LandingLogIn = () => {
+const SignInButton = () => {
   return (
-    <Button variant="light" size="lg" onClick={signInWithGoogle}>
-      Sign In
-    </Button>
+      <div className="sign-in-button">
+          <Button variant="light" size="lg" onClick={signInWithGoogle}>
+              Sign In
+          </Button>
+      </div>
+
   );
 };
 
@@ -19,13 +26,25 @@ const Landing = () => {
   return user ? (
     <Navigate to="/allEvents" />
   ) : (
-    <div>
-      <div>Find open events, join teams, and much more.</div>
-      <p>
-        MatchPoint lets you seamlessly find open sporting events and sign up for
-        them, join teams with your friends, and join leagues with your teams!
-      </p>
-      <LandingLogIn />
+    <div className="landing-page-container">
+        <div className="col-left">
+            {/*try hosting image*/}
+            <Image src={appImage} className="app-image"/>
+        </div>
+        <div className="col-right">
+            <div className="logo-container">
+                <Image src={smallLogo} className="small-logo"/>
+                <h1>MatchPoint</h1>
+            </div>
+
+            <h3>Find open events, join teams, and much more.</h3>
+            <p className="blurb">
+                MatchPoint lets you seamlessly find open sporting events and sign up for
+                them, join teams with your friends, and join leagues with your teams!
+            </p>
+            <SignInButton/>
+        </div>
+
     </div>
   );
 };
