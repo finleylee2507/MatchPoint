5d6246269c7d9093f344afcff8fbd85e6f6e485a
Vedant Apte <vvapte@ucdavis.edu>
2023-01-16 22:53:15 -0600

    join event functionality works with limitations

--- src/components/events/EventModal.jsx
+++ src/components/events/EventModal.jsx
@@ -1,32 +1,37 @@
-import React from 'react';
-import {Button, Modal} from 'react-bootstrap';
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
 
-function EventModal({show, handleClose, data,allUsers}) {
-    console.log("modal data: ", data);
-    const {name, location, maxCap, currCap, participants} = data;
-    console.log("participants: ", participants);
-    console.log("All users: ",allUsers);
-    return (
-        <Modal show={show} onHide={handleClose} info={data} centered backdrop="static">
-            <Modal.Header closeButton>
-                <Modal.Title>{name}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <p>Location: {location}</p>
-                <span>Participants: </span>
+function EventModal({ show, handleJoin, handleClose, data, allUsers }) {
+  console.log("modal data: ", data);
+  const { name, location, maxCap, currCap, participants, id, imgSrc } = data;
+  console.log("participants: ", participants);
+  console.log("All users: ", allUsers);
+  return (
+    <Modal
+      show={show}
+      onHide={handleClose}
+      info={data}
+      centered
+      backdrop="static"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>{name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>Location: {location}</p>
+        <span>Participants: </span>
 
-               {participants&&participants.map(id =>
-                    allUsers[id].displayName
-                ).join(", ")}
-                <p>Spots Available: {maxCap - currCap}</p>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="primary" onClick={handleClose}>
-                    Join Event
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+        {participants &&
+          participants.map((id) => allUsers[id].displayName).join(", ")}
+        <p>Spots Available: {maxCap - currCap}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={() => handleJoin(data)}>
+          Join Event
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default EventModal;
