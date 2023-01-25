a1a60c176269e37792cdf5dc1e4d9cb62ff61699
Vedant Apte <vvapte@ucdavis.edu>
2023-01-25 16:02:22 -0600

    User's events are now displaying

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -1,37 +1,43 @@
 // Profile Component
 import UserEventCard from "./UserEventCard";
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./Profile.css";
 
 const Profile = ({ allUsers, user, allEvents }) => {
-    // console.log(allUsers[user.uid]);
-    console.log(user);
-    if (user == undefined) {
-        return (
-            ""
-        )
-    }
+  console.log(user);
+  if (user == undefined || allUsers == undefined || allEvents == undefined) {
+    return "";
+  }
 
-    var currUser = allUsers[user.uid];
-    console.log(currUser);
+  const currUser = allUsers[user.uid];
 
-    // return (
-    //     ("events" in currUser || currUser[events]) ?
-    //         <div>
-    //             {userEvents = undefined || userEvents.length === 0 ? (
-    //                 <p className="empty-page-message">You are not a part of any events ...</p>
-    //             ) : (
-    //                 userEvents.map((e) => (
-    //                     <UserEventCard
-    //                         key={e}
-    //                         event={allEvents[e]}
-    //                     />
-    //                 ))
-    //             )}
-    //         </div>
-    //     :
-    //     ""
-    // )
-}
+  if ("events" in currUser) {
+    return currUser["events"].map((e) => (
+      <UserEventCard key={e} event={allEvents[e]} />
+    ));
+  } else {
+    return (
+      <p className="empty-page-message">You are not a part of any events ...</p>
+    );
+  }
+
+  // return (
+  //     ("events" in currUser || currUser[events]) ?
+  //         <div>
+  //             {userEvents = undefined || userEvents.length === 0 ? (
+  //                 <p className="empty-page-message">You are not a part of any events ...</p>
+  //             ) : (
+  //                 userEvents.map((e) => (
+  //                     <UserEventCard
+  //                         key={e}
+  //                         event={allEvents[e]}
+  //                     />
+  //                 ))
+  //             )}
+  //         </div>
+  //     :
+  //     ""
+  // )
+};
 
 export default Profile;
