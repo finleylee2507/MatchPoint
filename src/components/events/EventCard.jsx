0c24cfaf8a00907beb621450a0e7d2af9a9bb8cb
Finley Li <li.f@wustl.edu>
2023-01-30 14:54:30 -0600

    Major aesthetic update!

--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -1,199 +1,251 @@
 // Event Card
-import React from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Button, Card, Col, Row} from "react-bootstrap";
 import "./EventCard.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faAward,
-  faLocationDot,
-  faPeopleGroup,
-  faUserPlus,
-} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAward, faLocationDot, faPeopleGroup, faUserPlus,} from "@fortawesome/free-solid-svg-icons";
+import ellipsis from '../../assets/ellipsis.jpg';
 
 const EventCard = ({
-  openModal,
-  cardData,
-  openDeleteEventModal,
-  handleSetEventToDelete,
-  handleSetEventToEdit,
-  openEditEventModal,
-  user,
-  allUsers,
-  handleJoin,
-  handleLeave,
-}) => {
-  const { name, location, maxCap, imgSrc, participants, owner } = cardData;
-  // console.log("All users: ", allUsers);
-  const handleDelete = () => {
-    //pass to-be-deleted event to the parent component
-    handleSetEventToDelete(cardData);
+                       openModal,
+                       cardData,
+                       openDeleteEventModal,
+                       handleSetEventToDelete,
+                       handleSetEventToEdit,
+                       openEditEventModal,
+                       user,
+                       allUsers,
+                       handleJoin,
+                       handleLeave,
+                       handleSetEventToShowParticipants,
+                       handleShowParticipantsModal
+                   }) => {
+    const {name, location, maxCap, imgSrc, participants, owner} = cardData;
+    const [participantsToShow, setParticipantsToShow] = useState([]);
+    useEffect(() => {
+        if (cardData && allUsers) {
+            let tempList = [];
+            for (let i = 0; i < Math.min(5, cardData.participants.length); i++) {
+                tempList.push({
+                    photoURL: allUsers[cardData.participants[i]].photoURL,
+                    displayName: allUsers[cardData.participants[i]].displayName
+                });
+            }
+            setParticipantsToShow(tempList);
+        }
 
-    //show modal
-    openDeleteEventModal();
-  };
 
-  const handleEdit = () => {
-    //pass to-be-updated event to the parent component
-    handleSetEventToEdit(cardData);
+    });
+    console.log("All users: ", allUsers);
+    const handleDelete = () => {
+        //pass to-be-deleted event to the parent component
+        handleSetEventToDelete(cardData);
 
-    //open modal
-    openEditEventModal();
-  };
+        //show modal
+        openDeleteEventModal();
+    };
 
-  const handleJoinEvent = () => {
-    handleJoin(cardData);
-  };
 
-  if (allUsers == undefined || user == undefined) {
-    return "";
-  }
+    const handleEdit = () => {
+        //pass to-be-updated event to the parent component
+        handleSetEventToEdit(cardData);
 
-  return (
-    <Card className="card">
-      <Col>
-        <Row className="card-top">
-          <Col xs={4} className="circular-image-wrapper">
-            <img className="image" src={imgSrc} alt="image of event" />
-          </Col>
-          <Col>
-            <h2 className="text-muted">Date . Time</h2>
-            <Card.Title className="card-title">{name}</Card.Title>
-            <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
-            <a
-              href={`https://maps.google.com/?q=${cardData.location}`}
-              className="subheader"
-              target="_blank"
-            >
-              {" "}
-              Location: {cardData.location}
-            </a>
-          </Col>
-        </Row>
-        <hr className="solid"></hr>
-        <Row className="card-middle">
-          <Col>
-            <h3 className="details-header">Event Size</h3>
-            <div className="details">
-              <div className="left">
-                <FontAwesomeIcon icon={faPeopleGroup}></FontAwesomeIcon>
-              </div>
-              <div className="right">
-                <h3 className="details-text">{maxCap}</h3>
-              </div>
-            </div>
-          </Col>
-          <Col>
-            <h3 className="details-header">Spots Available</h3>
-            <div className="details">
-              <div className="left">
-                <FontAwesomeIcon icon={faUserPlus}></FontAwesomeIcon>
-              </div>
-              <div className="right">
-                <h3 className="details-text">{maxCap - participants.length}</h3>
-              </div>
-            </div>
-          </Col>
-          <Col>
-            <h3 className="details-header">Experience Level</h3>
-            <div className="details">
-              <div className="left">
-                <FontAwesomeIcon icon={faAward}></FontAwesomeIcon>
-              </div>
-              <div className="right">
-                <h3 className="details-text">{cardData.skillLevel}</h3>
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <div className="card-footer">
-          <Row>
-            <Col xs={2} className="circular-image-wrapper footer-padding">
-              {allUsers && allUsers[owner] && allUsers[owner].photoURL && (
-                <img
-                  className="profile-image"
-                  src={allUsers[owner].photoURL}
-                  alt="image of event"
-                />
-              )}
-            </Col>
-            <Col className="footer-padding">
-              <h2 className="subheader">Organized by</h2>
-              <h3 className="details-text">{allUsers[owner].displayName}</h3>
-            </Col>
-          </Row>
+        //open modal
+        openEditEventModal();
+    };
 
-          <Row>
+    const handleJoinEvent = () => {
+        handleJoin(cardData);
+    };
+
+
+    return (
+        <Card className="card">
             <Col>
-              <Button
-                className="card-button"
-                onClick={() => {
-                  openModal(cardData);
-                }}
-                variant="primary"
-                size="lg"
-              >
-                See more
-              </Button>
+                <Row className="card-top">
+                    <Col xs={4} className="circular-image-wrapper">
+                        <img className="image" src={imgSrc} alt="image of event"/>
+                    </Col>
+                    <Col>
+                        <Card.Title className="card-title">{name}</Card.Title>
+                        <h2 className="text-muted">Date . Time</h2>
+
+                        <div className="location">
+                            <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
+                            <a
+                                href={`https://maps.google.com/?q=${cardData.location}`}
+                                className="subheader"
+                                target="_blank"
+                            >
+                                {" "}
+                                Location: {cardData.location}
+                            </a>
+                        </div>
+
+                        <Button
+                            className="card-button see-more-button"
+                            onClick={() => {
+                                openModal(cardData);
+                            }}
+                            variant="primary"
+                            size="lg"
+                        >
+                            See more
+                        </Button>
+
+
+                    </Col>
+
+                    <Col>
+                        <Row>
+                            <Col xs={2} className="circular-image-wrapper footer-padding">
+                                {
+                                    allUsers && allUsers[owner] && allUsers[owner].photoURL && (
+                                        <img
+                                            className="owner-profile-image"
+                                            src={allUsers[owner].photoURL}
+                                            alt="image of event"
+                                        />
+                                    )
+                                }
+
+                            </Col>
+                            <Col className="footer-padding">
+
+                                <h2 className="subheader">Organized by</h2>
+
+
+                                <h3 className="details-text">{allUsers[owner].displayName}</h3>
+
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <h3 className="participant-title">Participants:</h3>
+                            <p className="participant-list-container" onClick={() => {
+                                handleShowParticipantsModal();
+                                handleSetEventToShowParticipants(cardData);
+                            }}>
+                                {participantsToShow.map(participant => {
+                                    let photoURL = participant.photoURL;
+                                    let displayName = participant.displayName;
+                                    return (
+                                        <img className="participant-profile-image" src={photoURL} alt={displayName}
+                                             title={displayName}/>);
+                                })}
+                                {
+                                    cardData.participants.length > 5 &&
+                                    <img className="participant-profile-image" src={ellipsis} title="more"/>
+                                }
+
+                            </p>
+                        </Row>
+                    </Col>
+                </Row>
+                <hr className="solid"></hr>
+                <Row className="card-middle">
+                    <Col>
+                        <h3 className="details-header">Event Size</h3>
+                        <div className="details">
+                            <div className="left">
+                                <FontAwesomeIcon icon={faPeopleGroup}></FontAwesomeIcon>
+                            </div>
+                            <div className="right">
+                                <h3 className="details-text">{maxCap}</h3>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <h3 className="details-header">Spots Available</h3>
+                        <div className="details">
+                            <div className="left">
+                                <FontAwesomeIcon icon={faUserPlus}></FontAwesomeIcon>
+                            </div>
+                            <div className="right">
+                                <h3 className="details-text">{maxCap - participants.length}</h3>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <h3 className="details-header">Experience Level</h3>
+                        <div className="details">
+                            <div className="left">
+                                <FontAwesomeIcon icon={faAward}></FontAwesomeIcon>
+                            </div>
+                            <div className="right">
+                                <h3 className="details-text">{cardData.skillLevel}</h3>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <div className="card-footer">
+                    <Row>
+
+                    </Row>
+
+                    <Row>
+                        <Col>
+
+                        </Col>
+
+                        {cardData.owner !== user.uid &&
+                            !cardData.participants.includes(user.uid) && (
+                                <Col>
+                                    <Button
+                                        className="card-button"
+                                        onClick={handleJoinEvent}
+                                        variant="primary"
+                                        size="lg"
+                                    >
+                                        Join Event
+                                    </Button>
+                                </Col>
+                            )}
+
+                        {owner === user.uid && (
+                            <Col>
+                                <Button
+                                    onClick={handleEdit}
+                                    variant="warning"
+                                    size="lg"
+                                    className="edit-event-button"
+                                >
+                                    Edit Event
+                                </Button>
+                            </Col>
+                        )}
+
+                        {owner === user.uid && (
+                            <Col>
+                                <Button
+                                    onClick={handleDelete}
+                                    variant="danger"
+                                    size="lg"
+                                    className="delete-event-button"
+                                >
+                                    Delete Event
+                                </Button>
+                            </Col>
+                        )}
+
+                        {cardData.participants.includes(user.uid) &&
+                            cardData.owner !== user.uid && (
+                                <Col>
+                                    <Button
+                                        size="lg"
+                                        onClick={() => {
+                                            handleLeave(cardData);
+                                        }}
+                                    >
+                                        Leave Event
+                                    </Button>
+                                </Col>
+                            )}
+                    </Row>
+                </div>
             </Col>
-
-            {cardData.owner !== user.uid &&
-              !cardData.participants.includes(user.uid) && (
-                <Col>
-                  <Button
-                    className="card-button"
-                    onClick={handleJoinEvent}
-                    variant="primary"
-                    size="lg"
-                  >
-                    Join Event
-                  </Button>
-                </Col>
-              )}
-
-            {owner === user.uid && (
-              <Col>
-                <Button
-                  onClick={handleEdit}
-                  variant="warning"
-                  size="lg"
-                  className="edit-event-button"
-                >
-                  Edit Event
-                </Button>
-              </Col>
-            )}
-
-            {owner === user.uid && (
-              <Col>
-                <Button
-                  onClick={handleDelete}
-                  variant="danger"
-                  size="lg"
-                  className="delete-event-button"
-                >
-                  Delete Event
-                </Button>
-              </Col>
-            )}
-
-            {cardData.participants.includes(user.uid) &&
-              cardData.owner !== user.uid && (
-                <Col>
-                  <Button
-                    size="lg"
-                    onClick={() => {
-                      handleLeave(cardData);
-                    }}
-                  >
-                    Leave Event
-                  </Button>
-                </Col>
-              )}
-          </Row>
-        </div>
-      </Col>
-    </Card>
-  );
+        </Card>
+    );
 };
 
 export default EventCard;
--- src/components/events/EventModal.jsx
+++ src/components/events/EventModal.jsx
@@ -1,128 +1,49 @@
-import React, { useState } from "react";
-import { Alert, Button, Modal, Table, Row } from "react-bootstrap";
+import React from "react";
+import {Modal} from "react-bootstrap";
 import "./EventModal.css";
-import logo from "../../assets/small-logo.png";
 
-function EventModal({ show, handleJoin, handleClose, data, allUsers }) {
-  // console.log("modal data: ", data);
-  const { name, location, maxCap, participants, dateTimeString, desc, owner } =
-    data;
-  const [shouldDisplayStatusMsg, setShouldDisplayStatusMsg] = useState(false);
-  const [statusMsg, setStatusMsg] = useState(null);
-  const dateTime = new Date(dateTimeString);
-  //console.log("participants: ", participants);
-  // console.log("All users: ", allUsers);
-  // console.log("Owner:",owner);
+function EventModal({show, handleJoin, handleClose, data}) {
+    if (!data) {
+        return "";
+    }
+    const {name, location, dateTimeString, desc,} = data;
 
-  const joinEventSuccessElement = (
-    <Alert key="success" variant="success">
-      Successfully joined event!
-    </Alert>
-  );
+    const dateTime = new Date(dateTimeString);
 
-  const joinEventFailureElementGeneric = (
-    <Alert key="danger" variant="danger">
-      Hmm something went wrong... Please check and try again.
-    </Alert>
-  );
+    return (
+        <Modal
+            show={show}
+            onHide={handleClose}
+            info={data}
+            centered
+            backdrop="static"
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>{name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>Description: {desc}</p>
+                <p>Location: {location}</p>
+                <p>
+                    Time:{" "}
+                    {dateTime.toLocaleString("en-US", {
+                        month: "long",
+                        day: "numeric",
+                        hour: "numeric",
+                        minute: "numeric",
+                        hour12: false,
+                        timeZone: "CST",
+                    })}{" "}
+                    CST
+                </p>
 
-  const joinEventFailureElementOwnEvent = (
-    <Alert key="danger" variant="danger">
-      Error: you can't join an event that you created!
-    </Alert>
-  );
+            </Modal.Body>
+            {/*<Modal.Footer>*/}
+            {/*  */}
 
-  const joinEventFailureElementParticipants = (
-    <Alert key="danger" variant="danger">
-      Error: you can't join an event that you're already a part of!
-    </Alert>
-  );
-
-  const joinEventFailureElementEventFull = (
-    <Alert key="danger" variant="danger">
-      Sorry the event has no available spots left...
-    </Alert>
-  );
-
-  return (
-    <Modal
-      show={show}
-      onHide={handleClose}
-      info={data}
-      centered
-      backdrop="static"
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>{name}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>Description: {desc}</p>
-        <p>Location: {location}</p>
-        <p>
-          Time:{" "}
-          {dateTime.toLocaleString("en-US", {
-            month: "long",
-            day: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-            hour12: false,
-            timeZone: "CST",
-          })}{" "}
-          CST
-        </p>
-        {/* {owner && <p>Owner: {allUsers[owner].displayName}</p>} */}
-        <span>Participants: </span>{" "}
-        {participants && participants.length == maxCap && (
-          <span>Event is full!</span>
-        )}
-        <Table className="modal-participant-list">
-          <tbody>
-            {participants &&
-              participants.map((id) => (
-                <tr className="participant-list-items">
-                  <td>
-                    <img
-                      className="profile-image-modal"
-                      src={allUsers[id].photoURL}
-                    />
-                  </td>
-                  <td className="participant-list-names">
-                    <span className="modal-participant-name">
-                      {allUsers[id].displayName} {id == owner ? "(Owner)" : ""}
-                    </span>
-                  </td>
-                </tr>
-              ))}
-            {participants &&
-              participants.length < maxCap &&
-              [...Array(maxCap - participants.length)].map((_, i) => (
-                <tr className="participant-list-items">
-                  <td>
-                    <img className="profile-image-modal" src={logo} />
-                  </td>
-                  <td className="participant-list-names">
-                    <span className="empty-participant-name">Empty spot</span>
-                  </td>
-                </tr>
-              ))}
-            {participants && participants.length == 0 && (
-              <tr>
-                <td>No participants yet!</td>
-              </tr>
-            )}
-          </tbody>
-        </Table>
-        {/* <p>
-          Spots Available:{" "}
-          {participants ? maxCap - participants.length : maxCap}
-        </p> */}
-      </Modal.Body>
-      {/*<Modal.Footer>*/}
-      {/*  */}
-
-      {/*</Modal.Footer>*/}
-    </Modal>
-  );
+            {/*</Modal.Footer>*/}
+        </Modal>
+    );
 }
 
 export default EventModal;
