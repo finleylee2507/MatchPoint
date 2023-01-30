9186bef44262d93375d18d3a5dd0c6e652a9b194
Vedant Apte <vvapte@ucdavis.edu>
2023-01-30 16:08:43 -0600

    Fixed user profile undefined bugs

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -7,42 +7,54 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 
 const UserEventList = ({ user, allEvents }) => {
-  return user["events"].map((e) => (
-    <UserEventCard key={e} event={allEvents[e]} />
-  ));
+  if (user["events"]) {
+    return user["events"].map((e) => (
+      <UserEventCard key={e} event={allEvents[e]} />
+    ));
+  } else {
+    return (
+      <div>
+        <p>
+          You don't have any upcoming events. Please create one, or join one
+          from the All Events tab!
+        </p>
+      </div>
+    );
+  }
 };
 const Profile = ({ allUsers, user, allEvents }) => {
-  // console.log(user);
-  if (
-    user == undefined ||
-    allUsers == undefined ||
-    allEvents == undefined ||
-    allUsers[user.uid]["events"] == undefined
-  ) {
-    return "";
+  if (user == undefined || allUsers == undefined || allEvents == undefined) {
+    return (
+      <div>
+        <p>Unable to display your profile. Please try again later.</p>
+      </div>
+    );
   }
-  var realUser = allUsers[user.uid];
-  console.log(realUser);
 
-  return (
-    <div className="user-profile-container">
-      <UserAvatar
-        imgSrc={
-          "https://as2.ftcdn.net/v2/jpg/02/15/84/43/1000_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
-        }
-      />
-      <p className="user-profile-title">{`Hi, ${realUser.displayName}`}</p>
-      <Tabs defaultActiveKey="upcoming" className="mb-3">
-        <Tab eventKey="upcoming" title="Upcoming">
-          <UserEventList user={realUser} allEvents={allEvents} />
-        </Tab>
+  // var realUser = allUsers[user.uid];
+  // console.log(realUser);
 
-        <Tab eventKey="past" title="Past"></Tab>
+  if (allUsers[user.uid]) {
+    return (
+      <div className="user-profile-container">
+        <UserAvatar
+          imgSrc={
+            "https://as2.ftcdn.net/v2/jpg/02/15/84/43/1000_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
+          }
+        />
+        <p className="user-profile-title">{`Hi, ${
+          allUsers[user.uid].displayName
+        }`}</p>
+        <Tabs defaultActiveKey="upcoming" className="mb-3">
+          <Tab eventKey="upcoming" title="Upcoming">
+            <UserEventList user={allUsers[user.uid]} allEvents={allEvents} />
+          </Tab>
 
-        <Tab eventKey="notifications" title="Notifications"></Tab>
-      </Tabs>
-    </div>
-  );
+          <Tab eventKey="past" title="Past"></Tab>
+        </Tabs>
+      </div>
+    );
+  }
 
   // if (allUsers[user.uid]["events"]) {
   //   return (
