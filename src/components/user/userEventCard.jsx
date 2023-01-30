3db44945bf9439546d807853c049ba437a4a05c0
Vedant Apte <vvapte@ucdavis.edu>
2023-01-30 16:16:45 -0600

    changed file name and prettier formatting

--- src/components/user/userEventCard.jsx
+++ src/components/user/userEventCard.jsx
@@ -1,39 +1,24 @@
-// Event Card
-import React from 'react';
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
-import './UserEventCard.css';
+// User Event Card
+import React from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import "./UserEventCard.css";
 
 const UserEventCard = ({ event }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{event.name}</Card.Title>
 
-    return (
-        <Card>
+        <Card.Text>date: something</Card.Text>
 
-            <Card.Body>
-                <Card.Title>
-                    { event.name }
-                </Card.Title>
+        <Card.Text>owner: {event.owner}</Card.Text>
 
-                <Card.Text>
-                    date: something 
-                </Card.Text>
+        <Card.Text>location: {event.location}</Card.Text>
 
-                <Card.Text>
-                    owner: { event.owner }
-                </Card.Text>
-
-                <Card.Text>
-                    location: { event.location }
-                </Card.Text>
-
-                <Card.Text>
-                    max capacity: { event.maxCap }
-                </Card.Text>
-                
-            </Card.Body>
-
-        </Card>
-    );
+        <Card.Text>max capacity: {event.maxCap}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
-
 
 export default UserEventCard;
