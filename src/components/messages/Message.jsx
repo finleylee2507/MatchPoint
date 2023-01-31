2856abd2c3d4ea80905aab8b9994382bc5cfbf83
Finley Li <li.f@wustl.edu>
2023-01-30 22:19:14 -0600

    Implemented delete message.

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Message.css";
 import { updateReadAndUnreadMessages } from "../../utilities/firebase";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Message = ({ message, allUsers, user }) => {
   if (message == undefined || allUsers == undefined || user == undefined) {
@@ -14,6 +16,7 @@ const Message = ({ message, allUsers, user }) => {
       return;
     }
     let updatedMessageData = {};
+
     if (!allUsers[user.uid].readMessages) {
       //user has no read messages
       // Update the list of unread messages
@@ -51,10 +54,61 @@ const Message = ({ message, allUsers, user }) => {
     }
   };
 
+  const deleteMessage = (e) => {
+    console.log("Deleting message...");
+    e.stopPropagation();
+
+    let updatedMessageData = {};
+
+    if (!allUsers[user.uid].readMessages) {
+      //user has no read messages
+      // Update the list of unread messages
+      let userUnreadMessages = allUsers[user.uid].unreadMessages;
+
+      userUnreadMessages = userUnreadMessages.filter(
+        (messageId) => messageId !== message.id
+      );
+      updatedMessageData.unreadMessages = userUnreadMessages;
+      updatedMessageData.readMessages = [];
+
+      // Call firebase function to update read and unread messages
+      updateReadAndUnreadMessages(updatedMessageData, user.uid);
+    } else {
+      //user has read messages
+
+      // Update the list of unread messages
+      let userUnreadMessages = allUsers[user.uid].unreadMessages;
+      let userReadMessages = allUsers[user.uid].readMessages;
+      userUnreadMessages = userUnreadMessages.filter(
+        (messageId) => messageId !== message.id
+      );
+
+      userReadMessages = userReadMessages.filter(
+        (messageId) => messageId !== message.id
+      );
+
+      updatedMessageData.unreadMessages = userUnreadMessages;
+      updatedMessageData.readMessages = userReadMessages;
+      // Call firebase function to update read and unread messages
+      updateReadAndUnreadMessages(updatedMessageData, user.uid);
+    }
+  };
+
   return (
     <div className="message" onClick={markAsRead}>
       <div>
-        <h1 className="card-title">{message.title}</h1>
+        <div className="message-upper-container">
+          <h1 className="card-title">{message.title}</h1>
+          {message.id !== "welcome" && (
+            <FontAwesomeIcon
+              icon={faTrashCan}
+              className="delete-message-icon"
+              size="lg"
+              onClick={deleteMessage}
+            />
+          )}
+        </div>
+
         <h3 className="subtitle">Event name</h3>
         <p className="message-content">{message.content}</p>
       </div>
