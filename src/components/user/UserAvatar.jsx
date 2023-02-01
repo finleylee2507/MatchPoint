d2ea3360315204184f09d64a734f27625952957b
Shiyuan Duan <shiyuanduan2023@u.northwestern.edu>
2023-01-31 21:51:52 -0600

    a

--- src/components/user/UserAvatar.jsx
+++ src/components/user/UserAvatar.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 
-export default function UserAvatar({ imgSrc }) {
+export default function UserAvatar({ imgSrc, width, height }) {
+  if(!(width && height)){
+    width = 150
+    height = 150
+  }
+  const _styles = {
+    container: {
+      overflow: "hidden",
+      borderRadius: "50%",
+      width: width,
+      height: height,
+    },
+    img: {
+      width: width,
+      height: height,
+    },
+  };
+  
   return (
     <div style={_styles.container}>
       <img style={_styles.img} src={imgSrc} />
     </div>
   );
+
 }
 
-const _styles = {
-  container: {
-    overflow: "hidden",
-    borderRadius: "50%",
-    width: 150,
-    height: 150,
-  },
-  img: {
-    width: 150,
-    height: 150,
-  },
-};
+
--- src/components/user/UserEventCard.jsx
+++ src/components/user/UserEventCard.jsx
@@ -1,10 +1,20 @@
 // User Event Card
 import React, {useState} from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import UserAvatar from "./UserAvatar";
 import "./UserEventCard.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAward,
+  faLocationDot,
+  faPeopleGroup,
+  faUserPlus,
+  faCircleInfo,
+  faUser,
+  faCalendar,
+} from "@fortawesome/free-solid-svg-icons";
 const UserEventCard = ({ event, displayName, allUsers, displayModalHook }) => {
-  
+  console.log(event)
   const handleOnClick = () => {
     
     displayModalHook({eventData:event, isShow:true})
@@ -12,15 +22,50 @@ const UserEventCard = ({ event, displayName, allUsers, displayModalHook }) => {
   return (
     <Card>
       <Card.Body className="user-event-card-body" onClick={handleOnClick}>
-        <Card.Title>{event.name}</Card.Title>
+        <Row>
+          <Col>
+            <UserAvatar imgSrc={event.imgSrc} width={100} height={100}/>
+          </Col>
+          
+          <Col>
+            <Card.Title>{event.name}</Card.Title>
+            {/* <Card.Text>{event.desc}</Card.Text> */}
 
-        <Card.Text>date: something</Card.Text>
+            <div className="card-info">
+              <FontAwesomeIcon
+                className="icon-width"
+                icon={faCalendar}
+                
+              />
+              <Card.Text>{event.dateTimeString}</Card.Text>
+            </div>
 
-        <Card.Text>owner: {allUsers[event.owner].displayName}</Card.Text>
+            <div className="card-info">
+              <FontAwesomeIcon
+                className="icon-width"
+                icon={faUser}
+              />
+              <Card.Text>{allUsers[event.owner].displayName}</Card.Text>
+            </div>
+            <div className="card-info">
+              <FontAwesomeIcon
+                className="icon-width"
+                icon={faLocationDot}
+              />
+              <Card.Text>{event.location}</Card.Text>
+            </div>
 
-        <Card.Text>location: {event.location}</Card.Text>
+              
+              {/* <Card.Text>owner: {allUsers[event.owner].displayName}</Card.Text>
+              <Card.Text>location: {event.location}</Card.Text>
+              <Card.Text>max capacity: {event.maxCap}</Card.Text> */}
 
-        <Card.Text>max capacity: {event.maxCap}</Card.Text>
+
+
+          </Col>
+        </Row>
+
+
       </Card.Body>
     </Card>
   );
