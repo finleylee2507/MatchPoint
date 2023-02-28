0754bb30d6042ba424978606458ac1e8287384df
Vedant Apte <vvapte@ucdavis.edu>
2023-02-28 10:57:27 -0600

    Added basic unit tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,15 @@
-import {describe, expect, test} from 'vitest';
-import {fireEvent, render, screen} from '@testing-library/react';
-import App from './App';
+import { describe, expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('counter tests', () => {
-    
-  test("Counter should be 0 at the start", () => {
+describe("Home Page Tests", () => {
+  test("Promotional message text should display", () => {
     render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+    expect(screen.getByText("Sporting events that pique their interests!"));
   });
 
-  test("Counter should increment by one when clicked", async () => {
+  test("MATCHPOINT text should display", () => {
     render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
+    expect(screen.getByText("MATCHPOINT"));
   });
-
 });
