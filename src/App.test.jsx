98de6b8de7d01f8875ac6a39f8e48cd6a21ee67a
Evan <evanmwaite@gmail.com>
2023-03-01 15:19:05 -0600

    initial commit

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,15 +1,15 @@
 import { describe, expect, test } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
+import Landing from "./App";
 
 describe("Home Page Tests", () => {
   test("Promotional message text should display", () => {
-    render(<App />);
+    render(<Landing />);
     expect(screen.getByText("Sporting events that pique their interests!"));
   });
 
   test("MATCHPOINT text should display", () => {
-    render(<App />);
+    render(<Landing />);
     expect(screen.getByText("MATCHPOINT"));
   });
 });
