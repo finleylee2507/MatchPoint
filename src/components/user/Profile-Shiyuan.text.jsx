51bb2c44b2d81ba7b6e7ae86587778d7ef8a8043
Shiyuan Duan <shiyuanduan2023@u.northwestern.edu>
2023-03-14 23:03:16 -0500

    shiyuan test

---
+++ src/components/user/Profile-Shiyuan.text.jsx
@@ -0,0 +1,45 @@
+import { describe, expect, it, vi } from "vitest";
+import {
+  fireEvent,
+  getByTestId,
+  getByText,
+  render,
+  waitFor,
+  screen,
+} from "@testing-library/react";
+import Profile from "./Profile";
+import { useProfile } from "../../utilities/userProfile";
+import { Nav } from "react-bootstrap";
+
+vi.mock("./utilities/userProfile");
+
+const mockUser = {
+    displayName: "mainUser",
+    email: "mainUser@gmail.com",
+    uid: "userId1",
+    photoURL: "abcdefg.com",
+  };
+  
+  const mockAllUsers = {
+    userId1: {
+      displayName: "user1",
+      email: "user1@gmail.com",
+      photoURL: "abcdefg.com",
+      unreadMessages: ["welcome"],
+    },
+};
+describe("This tests if user is not defined, profile should not be loaded", () => {
+    const { getByTestId, queryByTestId } = render(
+        <Profile
+          allUsers={null}
+          user={null}
+          allEvents={mockProfileEventsData}
+        />
+      );
+  
+      expect(
+        screen.getByText(
+          "Unable to load profile. Please try again later"
+        )
+      );
+})
