b10cc21660100863f72640a3826f1c2bd28d7a65
Finley Li <li.f@wustl.edu>
2023-11-07 01:41:45 +0800

    Updated landing page warning message.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,6 +15,8 @@ import img7 from "../assets/7.png";
 import img8 from "../assets/8.png";
 import logo from "../assets/MatchPoint-1.gif";
 import {FcGoogle} from "react-icons/fc";
+import React, {useEffect} from "react";
+import {toast, ToastContainer} from "react-toastify";
 
 const SignInButton = () => {
     return (
@@ -44,13 +46,11 @@ const ClearDBButton = () => {
 const Landing = (allUsers) => {
     const user = useAuthState();
 
-    // CHANGE UID AND DISPLAYNAME FOR CYPRESS TESTS
-    // const user = {
-    //   uid: "Faztbt70x8bKPMV3rJzco7fVvEr2",
-    //   displayName: "Vedant Apte",
-    //   photoURL: "",
-    // };
-
+    useEffect(() => {
+        toast.warning("Access to write features for this project is currently disabled, which may impact certain functionalities. To request access restoration, please reach out to the development team.",{
+            position: toast.POSITION.TOP_RIGHT,
+        })
+    }, []);
     if (user && allUsers && allUsers["allUsers"] && allUsers["allUsers"]) {
         if (!allUsers["allUsers"][user.uid]) {
             const newUser = {
@@ -67,35 +67,40 @@ const Landing = (allUsers) => {
     return user ? (
         <Navigate to="/allEvents"/>
     ) : (
-        <div className="landing-page-container">
-            <img className="landing-img1" src={img1} alt="landing page image"/>
-            <img className="landing-img2" src={img2} alt="landing page image"/>
-            <img className="landing-img3" src={img3} alt="landing page image"/>
-            <img className="landing-img4" src={img4} alt="landing page image"/>
-            <img className="landing-img5" src={img5} alt="landing page image"/>
-            <img className="landing-img6" src={img6} alt="landing page image"/>
-            <img className="landing-img7" src={img7} alt="landing page image"/>
-            <img className="landing-img8" src={img8} alt="landing page image"/>
-            <div className="col-right">
-                <div className="logo-container">
-                    <Image
-                        src={logo}
-                        className="small-logo"
-                        alt="small MatchPoint app gif logo"
-                    />
-                    <h1 className="app-name" data-cy="app-name">
-                        MatchPoint
-                    </h1>
-                </div>
+        <>
+            <ToastContainer autoClose={5000}/>
+            <div className="landing-page-container">
 
-                <h3 className="blurb">
-                    We help students create or join sporting events that pique their
-                    interests!
-                </h3>
-                <SignInButton/>
-                {/*<ClearDBButton />*/}
+                <img className="landing-img1" src={img1} alt="landing page image"/>
+                <img className="landing-img2" src={img2} alt="landing page image"/>
+                <img className="landing-img3" src={img3} alt="landing page image"/>
+                <img className="landing-img4" src={img4} alt="landing page image"/>
+                <img className="landing-img5" src={img5} alt="landing page image"/>
+                <img className="landing-img6" src={img6} alt="landing page image"/>
+                <img className="landing-img7" src={img7} alt="landing page image"/>
+                <img className="landing-img8" src={img8} alt="landing page image"/>
+                <div className="col-right">
+                    <div className="logo-container">
+                        <Image
+                            src={logo}
+                            className="small-logo"
+                            alt="small MatchPoint app gif logo"
+                        />
+                        <h1 className="app-name" data-cy="app-name">
+                            MatchPoint
+                        </h1>
+                    </div>
+
+                    <h3 className="blurb">
+                        We help students create or join sporting events that pique their
+                        interests!
+                    </h3>
+                    <SignInButton/>
+                    {/*<ClearDBButton />*/}
+                </div>
             </div>
-        </div>
+        </>
+
     );
 };
 
