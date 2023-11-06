24923d999d3cbc8ffff7883dc9cea1f74e4dabb2
Finley Li <li.f@wustl.edu>
2023-11-07 00:12:38 +0800

    Fixed .env error and workflow

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -13,13 +13,13 @@ import {deleteObject, getDownloadURL, getStorage, ref as sRef, uploadBytes,} fro
 
 // Firebase configuration dsata
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL:process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID
+    apiKey:import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL:import.meta.env.VITE_DATABASE_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase and database
