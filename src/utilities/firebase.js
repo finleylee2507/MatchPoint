6e7082efcb06db33320f9c0cb158618155e8a24e
Finley Li <li.f@wustl.edu>
2023-11-06 22:34:28 +0800

    Stored credentials in .env.

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -9,17 +9,17 @@ import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOu
 
 import {deleteObject, getDownloadURL, getStorage, ref as sRef, uploadBytes,} from "firebase/storage";
 
-import fireBaseConfig from "../../config";
+
 
 // Firebase configuration dsata
 const firebaseConfig = {
-    apiKey: fireBaseConfig.apiKey,
-    authDomain: fireBaseConfig.authDomain,
-    databaseURL: fireBaseConfig.databaseURL,
-    projectId: fireBaseConfig.projectId,
-    storageBucket: fireBaseConfig.storageBucket,
-    messagingSenderId: fireBaseConfig.messagingSenderId,
-    appId: fireBaseConfig.appId
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL:process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
 };
 
 // Initialize Firebase and database
